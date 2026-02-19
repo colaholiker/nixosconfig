7d8dab2f71d3d09ec3175cb05fee30bd62b29368
colaholiker <colaholiker@waskurzes.de>
2026-02-19 14:33:22 +0100

    hardware-config

--- configuration.nix
+++ configuration.nix
@@ -7,7 +7,7 @@
 {
   imports =
     [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
+      ./modules/hardware/asuspro_d520mt/hardware-configuration.nix
     ];
 
   # Bootloader.
