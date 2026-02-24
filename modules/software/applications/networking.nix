6d03a8620609fd871a03c5187a5750c470172413
colaholiker <colaholiker@waskurzes.de>
2026-02-24 10:36:21 +0100

    blub

--- modules/software/applications/networking.nix
+++ modules/software/applications/networking.nix
@@ -5,13 +5,10 @@
   ...
 }:
 let
-  gns3extras = pkgs.callPackage ../../../packages/gns3extras { };
-
   gnspkgs = with pkgs; [
     gns3-gui
     inetutils
     #ciscoPacketTracer8
-    gns3extras
   ];
   networkingpkgs = with pkgs; [
     arp-scan
@@ -37,32 +34,8 @@ lib.mkIf config.local.features.networking
   #];
   environment.systemPackages = gnspkgs ++ networkingpkgs;
 
-  # Run the gns3extras activation script on system activation so the
-  # symlinks in /var/lib/gns3 are created/updated automatically. We keep
-  # the invocation tolerant (|| true) so activation won't fail the whole
-  # activation if something about gns3 extras can't be applied.
-  system.activationScripts.gns3extras = {
-    supportsDryActivation = true;
-    text = ''
-      #!/bin/sh
-      # Only run if system uptime is at least 30 seconds
-      if [ $(cat /proc/uptime | cut -d. -f1) -ge 30 ]; then
-        # invoke activation script shipped inside the gns3extras package
-        ${gns3extras}/bin/gns3-extras-activate || true
-      else
-        echo "gns3extras activation script skipped due to uptime < 30s"
-      fi
-    '';
-  };
 
   services.atftpd.enable = true;
-
-  services.gns3-server = {
-    enable = true;
-    dynamips.enable = false;
-    ubridge.enable = true;
-    vpcs.enable = true;
-  };
 
   services.iperf3 = {
     enable = true;
